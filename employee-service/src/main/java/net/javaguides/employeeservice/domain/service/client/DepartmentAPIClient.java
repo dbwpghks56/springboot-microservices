ccc4c8cb89bee7d602a62b3dd5f835989de47e29
wpghks <wpghks56@naver.com>
2023-09-21 09:55:05 +0900

    [feature] apiclient 로 다른 서버 부를 때 spring cloud 이용해서 로드밸런서 사용하기

--- employee-service/src/main/java/net/javaguides/employeeservice/domain/service/client/DepartmentAPIClient.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/domain/service/client/DepartmentAPIClient.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "department-service", url = "http://localhost:8080/api/v1")
+// FeignClient는 내부적으로 로드밸런서를 사용하여 마이크로 서비스의 나머지 사용가능한 인스턴스 중 하나를 선택한다.
+@FeignClient(name = "DEPARTMENT-SERVICE/api/v1")
 public interface DepartmentAPIClient {
     // 이렇게 만들면 FeignClient가 자동으로 url을 붙여서 호출해준다.
     @GetMapping("/department/departmentCode/{departmentCode}")
