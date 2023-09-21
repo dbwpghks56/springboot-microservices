25a4f2f866ebaca7b107b97a302720c86befe05b
wpghks <wpghks56@naver.com>
2023-09-21 17:30:59 +0900

    [feature] spring cloud bus use

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/presentation/EmployeeMessageController.java
@@ -0,0 +1,18 @@
+package net.javaguides.employeeservice.presentation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class EmployeeMessageController {
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("/users/message")
+    public String getMessage() {
+        return message;
+    }
+}
