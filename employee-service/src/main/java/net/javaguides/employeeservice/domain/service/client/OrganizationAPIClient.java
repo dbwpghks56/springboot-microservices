78025766f98b58ccd8594536c7e370abbfbfa48c
wpghks <wpghks56@naver.com>
2023-09-26 11:49:17 +0900

    [chore] pathvariable add

--- employee-service/src/main/java/net/javaguides/employeeservice/domain/service/client/OrganizationAPIClient.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/domain/service/client/OrganizationAPIClient.java
@@ -4,9 +4,10 @@ import net.javaguides.employeeservice.common.CommonResponse;
 import net.javaguides.employeeservice.presentation.dto.response.OrganizationResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "ORGANIZATION-SERVICE/api/v1")
 public interface OrganizationAPIClient {
     @GetMapping("/organization/code/{organizationCode}")
-    CommonResponse<OrganizationResponseDto> findByOrganizationCode(String organizationCode);
+    CommonResponse<OrganizationResponseDto> findByOrganizationCode(@PathVariable String organizationCode);
 }
